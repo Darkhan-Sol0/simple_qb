4b382658df94d551bfe156ecee38ce20fcc69336
DarkhanSol0 <darkhansol069@gmail.com>
2025-12-18 14:00:51 +0900

    rework params

--- internal/params/params.go
+++ internal/params/params.go
@@ -2,7 +2,6 @@ package params
 
 import (
 	"fmt"
-	"reflect"
 	"strings"
 )
 
@@ -10,19 +9,18 @@ var whereTemplate = "WHERE %s"
 
 type (
 	node struct {
-		ncolumn   string
-		noperator string
-		nvalue    any
-		nlogic    string
+		ncolumn string
+		nquery  []string
+		nargs   []any
 	}
 
 	Node interface {
-		column() string
-		operator() string
-		value() any
-		logic() string
+		query(startIndex int) string
+		args() []any
 
 		Or() Node
+		And() Node
+
 		Eq(value any) Node
 		NotEq(value any) Node
 		Less(value any) Node
@@ -30,7 +28,7 @@ type (
 		Gr(value any) Node
 		GrEq(value any) Node
 		Like(value any) Node
-		In(value any) Node
+		In(value []any) Node
 		Null() Node
 		NotNull() Node
 	}
@@ -44,92 +42,96 @@ type (
 	}
 )
 
+func (n *node) query(startIndex int) string {
+	q := strings.Join(n.nquery, " ")
+	var a []any
+	for i := range n.nargs {
+		a = append(a, fmt.Sprint(i+1+startIndex))
+	}
+
+	return fmt.Sprintf(q, a...)
+}
+func (n *node) args() []any {
+	return n.nargs
+}
+
 func NewNode(column string) Node {
 	return &node{
-		ncolumn:   column,
-		noperator: "=",
-		nvalue:    nil,
-		nlogic:    "AND",
+		ncolumn: column,
+		nquery:  nil,
 	}
 }
 
 func (n *node) Eq(value any) Node {
-	n.noperator = "="
-	n.nvalue = value
+	n.nargs = append(n.nargs, value)
+	n.nquery = append(n.nquery, fmt.Sprintf("%s = %s", n.ncolumn, "$%s"))
 	return n
 }
 
 func (n *node) NotEq(value any) Node {
-	n.noperator = "<>"
-	n.nvalue = value
+	n.nargs = append(n.nargs, value)
+	n.nquery = append(n.nquery, fmt.Sprintf("%s <> %s", n.ncolumn, "$%s"))
 	return n
 }
 
 func (n *node) Less(value any) Node {
-	n.noperator = "<"
-	n.nvalue = value
+	n.nargs = append(n.nargs, value)
+	n.nquery = append(n.nquery, fmt.Sprintf("%s < %s", n.ncolumn, "$%s"))
 	return n
 }
 
 func (n *node) LessEq(value any) Node {
-	n.noperator = "<="
-	n.nvalue = value
+	n.nargs = append(n.nargs, value)
+	n.nquery = append(n.nquery, fmt.Sprintf("%s <= %s", n.ncolumn, "$%s"))
 	return n
 }
 
 func (n *node) Gr(value any) Node {
-	n.noperator = ">"
-	n.nvalue = value
+	n.nargs = append(n.nargs, value)
+	n.nquery = append(n.nquery, fmt.Sprintf("%s > %s", n.ncolumn, "$%s"))
 	return n
 }
 
 func (n *node) GrEq(value any) Node {
-	n.noperator = ">="
-	n.nvalue = value
+	n.nargs = append(n.nargs, value)
+	n.nquery = append(n.nquery, fmt.Sprintf("%s >= %s", n.ncolumn, "$%s"))
 	return n
 }
 
 func (n *node) Like(value any) Node {
-	n.noperator = "LIKE"
-	n.nvalue = value
+	n.nargs = append(n.nargs, value)
+	n.nquery = append(n.nquery, fmt.Sprintf("%s LIKE %s", n.ncolumn, "$%s"))
 	return n
 }
 
-func (n *node) In(value any) Node {
-	n.noperator = "IN"
-	n.nvalue = value
+func (n *node) In(value []any) Node {
+	var st []string
+	for range value {
+		st = append(st, "$%s")
+	}
+	n.nargs = append(n.nargs, value...)
+	n.nquery = append(n.nquery, fmt.Sprintf("%s IN (%s)", n.ncolumn, strings.Join(st, ", ")))
 	return n
 }
 
 func (n *node) Null() Node {
-	n.noperator = "IS NULL"
+	n.nquery = append(n.nquery, fmt.Sprintf("%s IS NULL", n.ncolumn))
 	return n
 }
 
 func (n *node) NotNull() Node {
-	n.noperator = "IS NOT NULL"
+	n.nquery = append(n.nquery, fmt.Sprintf("%s IS NOT NULL", n.ncolumn))
 	return n
 }
 
 func (n *node) Or() Node {
-	n.nlogic = "OR"
+	n.nquery = append(n.nquery, "OR")
 	return n
 }
 
-func (n *node) column() string {
-	return n.ncolumn
-}
-
-func (n *node) operator() string {
-	return n.noperator
-}
-
-func (n *node) value() any {
-	return n.nvalue
-}
-
-func (n *node) logic() string {
-	return n.nlogic
+func (n *node) And() Node {
+	n.nquery = append(n.nquery, "AND")
+	return n
 }
 
 // --------------------------------
@@ -141,54 +143,18 @@ func New(nodes ...Node) Params {
 }
 
 func (p *params) Generate(startIndex int) (query string, args []any) {
-	return p.getWhere(startIndex)
-}
-
-func (p *params) getWhere(startIndex int) (query string, args []any) {
 	if len(p.nodes) == 0 {
 		return "", nil
 	}
-	columns := make([]string, 2)
-	count := 1
-	for _, v := range p.nodes {
-		logic := v.logic()
-		operator := v.operator()
-		column := v.column()
-		val := v.value()
-		if operator != "" {
-			switch operator {
-			case "IN":
-				v := reflect.ValueOf(val)
-				if v.Kind() == reflect.Slice {
-					l := v.Len()
-					placeholders := make([]string, l)
-					for i := 0; i < l; i++ {
-						elem := v.Index(i)
-						placeholders[i] = fmt.Sprintf("$%d", startIndex+count)
-						args = append(args, elem.Interface())
-						count++
-					}
-					columns[1] = fmt.Sprintf("%s %s (%s)", column, operator, strings.Join(placeholders, ", "))
-				}
-			case "IS NULL", "IS NOT NULL":
-				columns[1] = fmt.Sprintf("%s %s", column, operator)
-
-			default:
-				columns[1] = fmt.Sprintf("%s %s $%d", column, operator, startIndex+count)
-				args = append(args, val)
-				count++
-			}
-			if columns[0] != "" {
-				text := strings.Join(columns, fmt.Sprintf(" %s ", logic))
-				columns[0] = text
-			} else {
-				columns[0] = columns[1]
-			}
-			columns[1] = ""
+	if len(p.nodes) == 1 {
+		return fmt.Sprintf(whereTemplate, p.nodes[0].query(startIndex+len(args))), args
+	}
+	var s []string
+	if len(p.nodes) > 1 {
+		for _, i := range p.nodes {
+			s = append(s, fmt.Sprintf("(%s)", i.query(startIndex+len(args))))
+			args = append(args, i.args()...)
 		}
 	}
-	if len(columns) == 0 {
-		return "", nil
-	}
-	return fmt.Sprintf(whereTemplate, columns[0]), args
+	return fmt.Sprintf(whereTemplate, strings.Join(s, " AND ")), args
 }
--- simple_qb_test.go
+++ simple_qb_test.go
@@ -1,5 +1,12 @@
 package simple_qb
 
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Darkhan-Sol0/simple_qb/internal/query"
+)
+
 type S struct {
 	Num  int    `db:"num"`
 	Text string `db:"text"`
@@ -7,74 +14,83 @@ type S struct {
 
 var table = "users"
 
-// func TestQueryInsert(t *testing.T) {
-// 	check := "INSERT INTO users (num, text) VALUES ($1, $2)"
-// 	s, arg := New(table).Insert(S{Num: 1, Text: "hui"}).Generate()
-// 	fmt.Println(s, arg)
-// 	if s != check {
-// 		t.Errorf("error: %s || %s", s, check)
-// 	}
-// }
+func TestQueryInsert(t *testing.T) {
+	check := "INSERT INTO users (num, text) VALUES ($1, $2)"
+	s, arg := New(table).Insert(S{Num: 1, Text: "hui"}).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
 
-// func TestQueryInsert1(t *testing.T) {
-// 	check := "INSERT INTO users (num, text) VALUES ($1, $2)"
-// 	s, arg := New(table).Insert(S{Num: 1}).Generate()
-// 	fmt.Println(s, arg)
-// 	if s != check {
-// 		t.Errorf("error: %s || %s", s, check)
-// 	}
-// }
+func TestQueryInsert1(t *testing.T) {
+	check := "INSERT INTO users (num, text) VALUES ($1, $2)"
+	s, arg := New(table).Insert(S{Num: 1}).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
 
-// func TestQuerySelect(t *testing.T) {
-// 	check := fmt.Sprintf("SELECT num, text FROM %s", table)
-// 	s, arg := New(table).Select(S{}).Generate()
-// 	fmt.Println(s, arg)
-// 	if s != check {
-// 		t.Errorf("error: %s || %s", s, check)
-// 	}
-// }
+func TestQuerySelect(t *testing.T) {
+	check := fmt.Sprintf("SELECT num, text FROM %s", table)
+	s, arg := New(table).Select(S{}).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
 
-// func TestQuerySelect1(t *testing.T) {
-// 	check := fmt.Sprintf("SELECT num, text FROM %s WHERE num IN ($1, $2, $3)", table)
-// 	s, arg := New(table).Select(S{}).Params(NewParam("num").In([]string{"1", "sad", "da11"})).Generate()
-// 	fmt.Println(s, arg)
-// 	if s != check {
-// 		t.Errorf("error: %s || %s", s, check)
-// 	}
-// }
+func TestQuerySelect1(t *testing.T) {
+	check := fmt.Sprintf("SELECT num, text FROM %s WHERE num IN ($1, $2, $3)", table)
+	s, arg := New(table).Select(S{}).Params(NewParam("num").In([]any{"1", "sad", "da11"})).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
 
-// func TestQuerySelect2(t *testing.T) {
-// 	check := fmt.Sprintf("SELECT num, text FROM %s WHERE num IN ($1, $2, $3)", table)
-// 	s, arg := New(table).Select(S{}).Params(NewParam("num").In([]string{"1", "sad", "da11"})).Generate()
-// 	fmt.Println(s, arg)
-// 	if s != check {
-// 		t.Errorf("error: %s || %s", s, check)
-// 	}
-// }
+func TestQuerySelect2(t *testing.T) {
+	check := fmt.Sprintf("SELECT num, text FROM %s WHERE num IN ($1, $2, $3)", table)
+	s, arg := New(table).Select(S{}).Params(NewParam("num").In([]any{"1", "sad", "da11"})).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
 
-// func TestQuerySelect3(t *testing.T) {
-// 	check := fmt.Sprintf("SELECT num, text FROM %s WHERE num = $1 OR num < $2", table)
-// 	s, arg := New(table).Select(S{}).Params(NewParam("num").Eq(1), NewParam("num").Less(5).Or()).Generate()
-// 	fmt.Println(s, arg)
-// 	if s != check {
-// 		t.Errorf("error: %s || %s", s, check)
-// 	}
-// }
+func TestQuerySelect3(t *testing.T) {
+	check := fmt.Sprintf("SELECT num, text FROM %s WHERE num = $1", table)
+	s, arg := New(table).Select(S{}).Params(NewParam("num").Eq(1)).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
 
-// func TestQuerySelect4(t *testing.T) {
-// 	check := fmt.Sprintf("SELECT COUNT(num) FROM %s WHERE num = $1 OR num < $2", table)
-// 	s, arg := New(table).Select(query.Count("num")).Params(NewParam("num").Eq(1), NewParam("num").Less(5).Or()).Generate()
-// 	fmt.Println(s, arg)
-// 	if s != check {
-// 		t.Errorf("error: %s || %s", s, check)
-// 	}
-// }
+func TestQuerySelect4(t *testing.T) {
+	check := fmt.Sprintf("SELECT COUNT(num) FROM %s WHERE num = $1 OR num < $2", table)
+	s, arg := New(table).Select(query.Count("num")).Params(NewParam("num").Eq(1).Or().Less(5)).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
 
-// func TestQuerySelect5(t *testing.T) {
-// 	check := fmt.Sprintf("SELECT COUNT(*) FROM %s", table)
-// 	s, arg := New(table).Select(query.Count("")).Generate()
-// 	fmt.Println(s, arg)
-// 	if s != check {
-// 		t.Errorf("error: %s || %s", s, check)
-// 	}
-// }
+func TestQuerySelect5(t *testing.T) {
+	check := fmt.Sprintf("SELECT COUNT(*) FROM %s", table)
+	s, arg := New(table).Select(query.Count("")).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
+
+func TestQueryUpdate(t *testing.T) {
+	check := fmt.Sprintf("UPDATE %s SET (num, text) = ROW($1, $2) WHERE (num = $3 OR num < $4) AND (text = $5 AND text LIKE $6)", table)
+	s, arg := New(table).Update(S{Num: 5, Text: "qwe"}).Params(NewParam("num").Eq(123).Or().Less(123), NewParam("text").Eq("asd").And().Like("hui")).Generate()
+	fmt.Println(s, arg)
+	if s != check {
+		t.Errorf("error: %s || %s", s, check)
+	}
+}
